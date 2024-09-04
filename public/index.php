8f2237a4fd41c60e2ac4d3ae5c1803abc95f3eb3
enzomu <enzomularczyk@hotmail.fr>
2024-09-04 10:37:47 +0200

    init composer and dependancies

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+($request->getPathInfo() === '/');
+
+$response = new Response('Hello, World!', Response::HTTP_OK);
+
+$response = new Response('Page not found', Response::HTTP_NOT_FOUND);
+
+$response->send();
