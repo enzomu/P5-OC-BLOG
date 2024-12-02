33efb435bc0b60e8deb841cb811f1ec585f8d563
enzomu <enzomularczyk@hotmail.fr>
2024-12-02 14:48:29 +0100

    fix .env and refacto dbmanager

--- src/Services/DbManager.php
+++ src/Services/DbManager.php
@@ -2,24 +2,33 @@
 
 namespace Enzo\P5OcBlog\Services;
 
+use Dotenv\Dotenv;
 use PDO;
 use PDOException;
 
 class DbManager
 {
-    private string $host = 'localhost';
-    private string $db = 'blog_db';
-    private string $user = 'root';
-    private string $pass = 'root';
+    private string $host;
+    private string $db;
+    private string $user;
+    private string $pass;
     private PDO $pdo;
 
     public function __construct()
     {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2), '.env');
+        $dotenv->load();
+
+        $this->host = $_ENV['DB_HOST'];
+        $this->db = $_ENV['DB_NAME'];
+        $this->user = $_ENV['DB_USER'];
+        $this->pass = $_ENV['DB_PASS'];
+
         try {
             $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset=utf8", $this->user, $this->pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
+            echo 'Connection failed';
         }
     }
 
